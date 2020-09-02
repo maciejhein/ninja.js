a58441f4bad40ee55c8d277a37df637ae82fbcb5
maciejhein <maciej.hein@acoustic.co>
2020-09-02 19:48:59 +0200

    refactor pagination

---
+++ src/DataTable/Pagination/Pagination.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import PaginationItems from './PaginationItems'
+
+const Pagination = ({ currentPageNumber, totalNumberOfPages, onChange }) => {
+  if(totalNumberOfPages <= 1){
+    return null;
+  }
+
+  const pages =Array.from(Array(totalNumberOfPages).keys())
+
+  return(
+    <ul className="pagination">
+      {
+        pages.map(pageNumber => {
+          return <PaginationItems
+            key={pageNumber}
+            isActive={currentPageNumber === pageNumber}
+            currentPageNumber={currentPageNumber}
+            pageNumber={pageNumber}
+            onChange={onChange} />
+        })
+      }
+    </ul>
+  )
+}
+
+export default Pagination
---
+++ src/DataTable/Pagination/PaginationItems/PaginationItems.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const PaginationItem = ({ pageNumber, isActive, onChange }) => {
+  const classIsActive = isActive ? 'button-outline' : '';
+
+  const onClick = (event) => {
+    event.preventDefault();
+    
+    onChange(pageNumber);
+  };
+
+  return(
+    <li className="page-item mr-1">
+      <button className={`page-link ${classIsActive}`} onClick={onClick} >{pageNumber + 1}</button>
+    </li>
+  )
+}
+
+export default PaginationItem
